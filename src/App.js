40a204b5ce59ce8a4c9ca0b2be991e1ceef007eb
aparnadevigm <aparnach1269@gmail.com>
2024-06-21 22:58:35 +0530

    first-commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="text-3xl fomt-bold text-green-800">
+      Om Namah Shivaya om Namah Shivaya om Namah Shivaya om 
+    </div>
+  );
+}
+
+export default App;
